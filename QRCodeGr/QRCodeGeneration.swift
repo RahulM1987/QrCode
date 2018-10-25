5e6c615fe70a0eec8c8c13f2d68a76ad1c5d20fd
Rahul Morade <rahulmorade@Rahuls-Mac-mini.local>
2018-10-25 17:32:52 +0530

    first commit

---
+++ QRCodeGr/QRCodeGeneration.swift
@@ -0,0 +1,13 @@
+//
+//  QRCodeGeneration.swift
+//  QRCodeGr
+//
+//  Created by Rahul Morade on 24/10/18.
+//  Copyright © 2018 Rahul Morade. All rights reserved.
+//
+
+import UIKit
+
+class QRCodeGeneration: NSObject {
+
+}
